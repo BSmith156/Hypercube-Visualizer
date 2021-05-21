b55743f6e267fbd0374c612440a1b2220fd771e4
Ben Smith <ben.smith-8@student.manchester.ac.uk>
2021-05-21 11:18:03 +0100

    Improve resize speed

--- js/onResize/resizeCanvas.js
+++ js/onResize/resizeCanvas.js
@@ -1,7 +1,8 @@
+const canvas = document.getElementById("canvas");
+const above = document.getElementById("navbar");
+const left = document.getElementById("controls");
+
 export function resizeCanvas() {
-    const canvas = document.getElementById("canvas");
-    const above = document.getElementById("navbar");
-    const left = document.getElementById("controls");
     canvas.height = (canvas.getBoundingClientRect().bottom) - (above.getBoundingClientRect().bottom + parseInt(window.getComputedStyle(canvas).marginTop));
     canvas.width = (canvas.getBoundingClientRect().right) - (left.getBoundingClientRect().right + parseInt(window.getComputedStyle(left).marginRight))
 }
--- js/onResize/resizeControls.js
+++ js/onResize/resizeControls.js
@@ -1,5 +1,6 @@
+const controls = document.getElementById("controls");
+const above = document.getElementById("navbar");
+
 export function resizeControls() {
-    const controls = document.getElementById("controls");
-    const above = document.getElementById("navbar");
     controls.style.height = (canvas.getBoundingClientRect().bottom) - (above.getBoundingClientRect().bottom + parseInt(window.getComputedStyle(controls).marginTop));
 }
