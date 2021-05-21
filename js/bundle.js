d9b916217e92b871fd26ae24dcb760b8ed862043
Ben Smith <ben.smith-8@student.manchester.ac.uk>
2021-05-21 15:05:40 +0100

    Add canvas refreshing

--- js/bundle.js
+++ js/bundle.js
@@ -1,11 +1,14 @@
 import { onResize } from './onResize/onResize.js';
 import { Hypercube } from './hypercube.js';
 import { draw } from './draw.js';
+const fps = 30;
 
 window.onload = () => {
     onResize();
     let hypercube = new Hypercube(3, []);
-    draw(hypercube);
+    setInterval(() => {
+        draw(hypercube);
+    }, 1000 / fps);
     window.onresize = () => {
         onResize();
         hypercube.setZoom();
